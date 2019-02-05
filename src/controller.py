38151e436adc13dab48b88946789923c5c0dfbe2
Nikolay Sokolov <nikolay.sokolov@neulion.com>
2019-02-05 12:02:49 +0700

    made sql connection as singleton

--- src/controller.py
+++ src/controller.py
@@ -5,28 +5,27 @@ from src.sql import Sql
 class Controller:
 
     def __init__(self):
+        self.sql = None
         self.connect_signals()
 
     def connect_signals(self):
         signals.make_query.connect(self.make_query)
 
-    @staticmethod
-    def make_query(connection, query):
+    def make_query(self, connection, query):
         """
 
         :param str connection:
         :param str query:
         :return:
         """
-        sql = Sql(connection)
+        self.sql = Sql(connection)
         try:
             if query.lower().startswith('select'):
-                header, result = sql.select(query)
+                header, result = self.sql.select(query)
                 signals.draw_table.emit(header, result)
-            elif query.lower().startswith('create'):
-                sql.create(query)
-            elif query.lower().startswith('insert'):
-                sql.insert(query)
-
+            elif query.lower().startswith('create') or query.lower().startswith('insert'):
+                self.sql.create_insert(query)
+            else:
+                signals.error_received('Unknown query')
         except Exception as err:
             signals.error_received.emit(f'{err}')
--- src/sql.py
+++ src/sql.py
@@ -1,36 +1,33 @@
 import sqlite3
 
 
+def singleton(cls):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return getinstance
+
+
+@singleton
 class Sql:
 
     def __init__(self, filename):
-        self.filename = filename
-        self.connect = None
+        self.connect = sqlite3.connect(filename)
         self.cursor = None
 
     def __del__(self):
         self._commit()
 
-    def _connect(self):
-        self.connect = sqlite3.connect(self.filename)
+    def create_insert(self, query):
         self.cursor = self.connect.cursor()
-
-    def _commit(self):
+        self.cursor.execute(query)
         self.connect.commit()
-        self.connect.close()
-
-    def create(self, query):
-        self._connect()
-        self.cursor.execute(query)
-        self._commit()
-
-    def insert(self, query):
-        self._connect()
-        self.cursor.execute(query)
-        self._commit()
 
     def select(self, query):
-        self._connect()
+        self.cursor = self.connect.cursor()
         self.cursor.execute(query)
         headers = [item[0] for item in self.cursor.description]
         return headers, self.cursor.fetchall()
